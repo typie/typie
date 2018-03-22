a271fd88f6fa26d9a7260515014e32d8b85f3589
Rotem <rotem.g@datarails.io>
2018-03-22 20:07:36 +0200

    add polymer

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,216 +2,26 @@ const path = require('path');
 
 // just some stuff for playing - feel free to delete
 'use strict';
-const styles=document.createElement('style');
-styles.innerText=`@import url(https://unpkg.com/spectre.css/dist/spectre.min.css);
-.empty{display:flex;flex-direction:column;justify-content:center;height:100vh;position:relative}.footer{bottom:0;font-size:13px;left:50%;opacity:.9;position:absolute;transform:translateX(-50%);width:100%}`;
 
+// const styles=document.createElement('style');
+// styles.innerText=`@import url(https://unpkg.com/spectre.css/dist/spectre.min.css);
+// .empty{display:flex;flex-direction:column;justify-content:center;height:100vh;position:relative}.footer{bottom:0;font-size:13px;left:50%;opacity:.9;position:absolute;transform:translateX(-50%);width:100%}`;
 
-const cssLink=document.createElement('link');
-cssLink.setAttribute('rel','stylesheet');
-cssLink.setAttribute('href', "themes/default/style.css");
+const polymer=document.createElement('script');
+polymer.setAttribute('type','text/javascript');
+polymer.setAttribute('src','polymer/bower_components/webcomponentsjs/webcomponents-loader.js');
 
-const vueScript=document.createElement('script');
-vueScript.setAttribute('type','text/javascript');
-vueScript.setAttribute('src','https://unpkg.com/vue');
+const importElement=document.createElement('link');
+importElement.setAttribute('rel','import');
+importElement.setAttribute('href','polymer/haste-search.html');
+
+const hasteSearch=document.createElement('haste-search');
+
 //vueScript.onload = init;
-document.head.appendChild(cssLink);
-document.head.appendChild(vueScript);
-document.head.appendChild(styles);
-
-const {remote, ipcRenderer} = require('electron');
-const round = require('vue-round-filter');
-const Vue = require('vue/dist/vue.common');
-const MainRendererWindow = require('./MainRendererWindow');
+document.head.appendChild(polymer);
+document.head.appendChild(importElement);
 
 
-let packages = {};
-let win = new MainRendererWindow(remote.getCurrentWindow());
+document.body.appendChild(hasteSearch);
 
-function init(){
-    Vue.config.devtools = false;
-    Vue.config.productionTip = false;
 
-    let mainVue = new Vue({
-        el: 'body',
-        data: {
-            versions: {
-                electron: process.versions.electron,
-                electronWebpack: require('electron-webpack/package.json').version
-            },
-            defaultPackage: 'global',
-            searchVal: '',
-            list: [],
-            selected: null,
-            currentPackage: 'global',
-            total: 0,
-            workers: 0,
-            pkgs: packages,
-            visible: true
-        },
-        watch: {
-            'searchVal': function (val, oldVal) {
-                console.log('searching');
-                this.searchIt(val, this.currentPackage);
-            },
-            'package': function (val, oldVal) {
-                this.list = [];
-                this.searchVal = '';
-                this.selected = 0;
-                this.refreshList();
-            },
-            'list': function () {
-                this.selected = 0;
-                //win.resize();
-            }
-        },
-        methods: {
-            open(b){
-                require('electron').shell.openExternal(b)
-            },
-            searchIt: (val, currentPackage) => {
-                if (val !== '') {
-                    // this.selected = null
-                    ipcRenderer.send('search', {value: val, currentPackage: currentPackage});
-                } else {
-                    this.refreshList();
-                }
-            },
-            keyEventTest: function (event) {
-                // console.log(event)
-            },
-            down: function (event) {
-                if (this.selected < this.list.length - 1) {
-                    this.selected++;
-                }
-            },
-            up: function (event) {
-                if (this.selected > 0) {
-                    this.selected--;
-                }
-            },
-            enter: function (e) {
-                let row = this.list[this.selected];
-                if (row !== undefined) {
-                    let newPackage = row.title;
-                    ipcRenderer.send('activateRow', {
-                        newPackage: newPackage,
-                        selectedRow: row,
-                        pressedKey: e.code
-                    })
-                }
-            },
-            click: function (item) {
-                ipcRenderer.send('activateRow', {
-                    newPackage: item.title,
-                    selectedRow: item,
-                    pressedKey: 'Click'
-                })
-                win.focus()
-            },
-            tab: function (e) {
-                this.enter(e)
-            },
-            backspace: function (event) {
-                ipcRenderer.send('backspace', {
-                    key: event.key,
-                    searchVal: this.searchVal,
-                    selectedRow: this.list[this.selected]
-                })
-            },
-            esc: function () {
-                this.currentPackage = this.defaultPackage;
-                this.searchVal = '';
-                win.hide();
-            },
-            close: function () {
-                this.esc()
-                // win.close()
-            },
-            refreshList: function () {
-                ipcRenderer.send('refreshList');
-            }
-        },
-        filters: {
-            round
-        },
-        template: `<div>
-        <div class="icon"></div>
-        <div class="search">
-            <span class="prefix" v-if="currentPackage!=defaultPackage">{{currentPackage}}</span>
-            <input autofocus id="inputField" type="text"
-                   v-model="searchVal"
-                   @keydown.stop.down.prevent="down"
-                   @keydown.stop.up.prevent="up"
-                   @keyup.stop.enter.prevent="enter"
-                   @keydown.stop.tab.prevent="tab"
-                   @keyup.stop.space="tab"
-    
-                   @keydown.delete="backspace"
-                   @keydown.esc="esc"
-                   @keydown="keyEventTest"
-                   placeholder=""/>
-            <div class="close" v-on:click="close">x</div>
-        </div>
-        <ul class="results" v-bind:class="">
-            <li v-for="(item, index) in list" v-bind:class="{selected: selected===index}" v-on:click="click(item)">
-                <img v-bind:src="item.icon" v-if="item.icon">
-                <div class="texts">
-                    <span>{{ item.title }}</span>
-                    <p v-if="item.d !== undefined">{{item.d}}</p>
-                    <p v-else>{{item.path}}</p>
-                </div>
-                <div class="score">
-                    {{item.score | round}}<span v-if="item.called"> + {{item.called}}</span>
-                </div>
-            </li>
-        </ul>
-        <div class="footer">
-            <div class="handle">· · · ·</div>
-            <div class="status"><span v-if="workers > 0"> items in Catalog · Haste 1.0</span>
-            </div>
-        </div>
-        </div>`
-
-        //  components: { App }
-    }).$mount('#app');
-
-    ipcRenderer.on('resultList', (event, list) => {
-        mainVue.list = [];
-        Object.keys(list).forEach(function(key) {
-            mainVue.list.push(list[key]);
-        });
-        console.log(mainVue.list);
-        if (mainVue.selected === null && mainVue.list.length > 0) {
-            mainVue.selected = 0
-        }
-    });
-    ipcRenderer.on('changePackage', (event, arg) => {
-        mainVue.currentPackage = arg
-        mainVue.searchVal = ''
-        mainVue.refreshList()
-    });
-    ipcRenderer.on('refreshList', (event, arg) => {
-        mainVue.list = arg[0]
-        mainVue.total = arg[1]
-    });
-    ipcRenderer.on('console', (event, arg) => {
-        console.log(arg)
-    });
-    win.win.on('blur', (e, cmd) => {
-        win.hide()
-    });
-    win.win.on('show', (e, cmd) => {
-        setTimeout(function () {
-            mainVue.visible = true;
-            document.getElementById('inputField').focus()
-        }, 10)
-    });
-    win.setVue(mainVue);
-    win.registerEvents();
-    win.registerListeningEvents();
-    ipcRenderer.send('refreshList');
-
-}
-
-init();
