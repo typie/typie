ad03ae8d8ddc15d51a2c4f794b5980fd51a3c806
rotem <rotemgrim@gmail.com>
2019-02-28 19:24:14 +0200

    update typie to use electron 4

--- static/packages/SwitchTo/index.js
+++ static/packages/SwitchTo/index.js
@@ -6,25 +6,24 @@ class SwitchTo extends AbstractTypiePackage {
         super(win, config, pkgPath);
         this.win         = win;
         this.packageName = 'SwitchTo';
-        this.intervalTime  = 250; // milliseconds -> do not lower below 250
-        this.watchInterval = null;
-        this.lastBatchHash = null;
-        this.startWatch();
+        // this.intervalTime  = 250; // milliseconds -> do not lower below 250
+        // this.watchInterval = null;
+        // this.lastBatchHash = null;
+        // this.startWatch();
     }
 
-    insert(value, description="", path="", icon="") {
-        let item = this.getDefaultItem(value, description, path, icon);
-        item.setDescription("Activate to Switch");
-        this.insertItem(item);
-    }
+    // insert(value, description="", path="", icon="") {
+    //     let item = this.getDefaultItem(value, description, path, icon);
+    //     item.setDescription("Activate to Switch");
+    //     this.insertItem(item);
+    // }
 
     activate(pkgList, item, cb) {
         this.win.hide();
-        this.typie.updateCalled(item).go()
-            .then(()=>{
-                this.typie.pasteText().go();
-            })
-            .catch(()=>{});
+        setTimeout(() => {
+            console.log(item);
+            this.typie.remove(item).go()
+        }, 2000);
     }
 
     // remove(pkgList, item, cb) {
@@ -45,19 +44,19 @@ class SwitchTo extends AbstractTypiePackage {
             .catch(err => console.log(err));
     }
 
-    startWatch() {
-        this.watchInterval = setInterval(() => {
-            // let content = clipboard.readText();
-            // if (this.lastPaste !== content) {
-            //     this.lastPaste = content;
-            //     this.insert(content);
-            // }
-        }, this.intervalTime);
-    }
+    // startWatch() {
+    //     this.watchInterval = setInterval(() => {
+    //         // let content = clipboard.readText();
+    //         // if (this.lastPaste !== content) {
+    //         //     this.lastPaste = content;
+    //         //     this.insert(content);
+    //         // }
+    //     }, this.intervalTime);
+    // }
 
     destroy() {
         super.destroy();
-        clearInterval(this.watchInterval);
+        //clearInterval(this.watchInterval);
     }
 }
 module.exports = SwitchTo;
